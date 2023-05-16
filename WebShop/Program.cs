289d1088cacded6159c872b086b8571cd6dd8a82
DanTran <datt4347@gmail.com>
2023-05-16 22:07:42 +0700

    change folder and done  project

---
+++ WebShop/Program.cs
@@ -0,0 +1,70 @@
+﻿using AspNetCoreHero.ToastNotification;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using WebShop.Models;
+// Configure Service
+var builder = WebApplication.CreateBuilder(args);
+
+var connectString = builder.Configuration.GetConnectionString("WebShopConnectionString");
+builder.Services.AddDbContext<dbMarketsContext>(options => options.UseSqlServer(connectString));
+
+builder.Services.AddSingleton<HtmlEncoder>(HtmlEncoder.Create(allowedRanges: new[] { UnicodeRanges.All }));
+builder.Services.AddSession();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(p =>
+    {
+        p.Cookie.Name = "UserLoginCookie";
+        p.ExpireTimeSpan = TimeSpan.FromDays(1);
+        //p.LoginPath = "/dang-nhap.html";
+        //p.LogoutPath = "/dang-xuat/html";
+        p.AccessDeniedPath = "/not-found.html";
+    });
+
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+builder.Services.AddNotyf(config =>
+{
+    config.DurationInSeconds = 3;
+    config.IsDismissable = true;
+    config.Position = NotyfPosition.TopRight;
+});
+
+var app = builder.Build();
+
+// Configure method in startup
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseSession();
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "areas",
+        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
+app.Run();
